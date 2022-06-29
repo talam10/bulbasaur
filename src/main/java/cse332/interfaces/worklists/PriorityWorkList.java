f1e21a9573e4b6ba8f4c5f837ec95dfd198dca97
talam10 <108381006+talam10@users.noreply.github.com>
2022-06-28 22:23:51 -0700

    Add files via upload

---
+++ src/main/java/cse332/interfaces/worklists/PriorityWorkList.java
@@ -0,0 +1,13 @@
+package cse332.interfaces.worklists;
+
+/**
+ * A subclass of WorkList that stores its elements in priority order. All
+ * subclasses of this class implicitly agree to the contract to be a priority
+ * queue. The "priority" of an element should be determined via compareTo(). The
+ * direction (min or max) is up to the implementation.
+ *
+ * @param <E> the type of elements in the worklist; must be comparable
+ * @author Adam Blank
+ */
+public abstract class PriorityWorkList<E> extends WorkList<E> {
+}
